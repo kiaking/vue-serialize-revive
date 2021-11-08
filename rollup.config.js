558d223a086b0bc4ce0a5201ff150cf5976e13f9
Kia King Ishii <kia.king.08@gmail.com>
2021-11-08 19:18:57 +0900

    build: add release script

---
+++ rollup.config.js
@@ -0,0 +1,70 @@
+import path from 'path'
+import replace from '@rollup/plugin-replace'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import ts from 'rollup-plugin-typescript2'
+import { terser } from 'rollup-plugin-terser'
+
+const configs = [
+  { input: 'src/index.ts', file: 'dist/vue-serialize-revive.esm-browser.js', format: 'es', browser: true, env: 'development' },
+  { input: 'src/index.ts', file: 'dist/vue-serialize-revive.esm-browser.prod.js', format: 'es', browser: true, env: 'production' },
+  { input: 'src/index.ts', file: 'dist/vue-serialize-revive.esm-bundler.js', format: 'es', env: 'development' },
+  { input: 'src/index.ts', file: 'dist/vue-serialize-revive.global.js', format: 'iife', env: 'development' },
+  { input: 'src/index.ts', file: 'dist/vue-serialize-revive.global.prod.js', format: 'iife', minify: true, env: 'production' },
+  { input: 'src/index.ts', file: 'dist/vue-serialize-revive.cjs.js', format: 'cjs', env: 'development' }
+]
+
+function createEntries() {
+  return configs.map((c) => createEntry(c))
+}
+
+function createEntry(config) {
+  const c = {
+    input: config.input,
+    plugins: [],
+    output: {
+      file: config.file,
+      format: config.format
+    },
+    onwarn: (msg, warn) => {
+      if (!/Circular/.test(msg)) {
+        warn(msg)
+      }
+    }
+  }
+
+  if (config.format === 'iife') {
+    c.output.name = 'VueSerializeRevive'
+  }
+
+  c.plugins.push(replace({
+    __DEV__: config.format === 'es' && !config.browser
+      ? `(process.env.NODE_ENV !== 'production')`
+      : config.env !== 'production',
+    preventAssignment: true
+  }))
+
+  c.plugins.push(resolve({ browser: true }))
+  c.plugins.push(commonjs())
+
+  c.plugins.push(ts({
+    check: config.format === 'es' && config.browser && config.env === 'development',
+    tsconfig: path.resolve(__dirname, 'tsconfig.json'),
+    cacheRoot: path.resolve(__dirname, 'node_modules/.rts2_cache'),
+    tsconfigOverride: {
+      compilerOptions: {
+        declaration: config.format === 'es' && config.browser && config.env === 'development',
+        target: config.format === 'iife' || config.format === 'cjs' ? 'es5' : 'es2018'
+      },
+      exclude: ['__tests__']
+    }
+  }))
+
+  if (config.minify) {
+    c.plugins.push(terser({ module: config.format === 'es' }))
+  }
+
+  return c
+}
+
+export default createEntries()
